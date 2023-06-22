8cb4bd462f3e9cd81dcb08a6b393f22164de5ba3
ifyfaith24 <ifyfaith24@gmail.com>
2023-06-22 13:24:28 -0700

    monty project

--- pop.c
+++ pop.c
@@ -1,15 +1,25 @@
 #include "monty.h"
-
 /**
- * pop - checks fail case for deleting top node from stack
- * @stack: double pointer to stack
- * @line_number: current error line
- */
-void pop(stack_t **stack, unsigned int line_number)
+  * pop_func - delete note at top of stack
+  * @stack: pointer to head of stack
+  * @line_number: line number
+  */
+void pop_func(stack_t **stack, unsigned int line_number)
 {
-	if (del_top_node(stack) == 1)
+	stack_t *tmp = (*stack);
+
+	if (*stack)
 	{
-		printf("L%d: can't pop an empty stack\n", line_number);
+		(*stack) = (*stack)->next;
+		free(tmp);
+		if (*stack)
+			(*stack)->prev = NULL;
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		fclose(global.fd);
+		free(global.opcode);
 		exit(EXIT_FAILURE);
 	}
 }
