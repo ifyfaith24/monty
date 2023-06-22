cbc026ba72e2e2b5393d695c7f74b17d9d7ba681
ifyfaith24 <ifyfaith24@gmail.com>
2023-06-22 11:16:45 -0700

    monty project

--- pop.c
+++ pop.c
@@ -12,7 +12,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || stack == NULL)
 	{
-		printf("L%u: can't pop an empty stack\n", ln);
+		printf("L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	temp = *stack;
