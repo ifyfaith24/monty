9b6648046c85e48fd0738c5a4bfc3bf6c7be23ac
ifyfaith24 <ifyfaith24@gmail.com>
2023-06-22 12:19:52 -0700

    monty project

--- swap.c
+++ swap.c
@@ -1,29 +1,21 @@
 #include "monty.h"
 
 /**
- * swap - swaps the top two elements of the stack
- * @stack: pointer to the given node
- * @line_number: line number
- * Return: nothing
+ * swap - swaps two elements of a stack
+ * @stack: double pointer to stack
+ * @line_number: current error line
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-	int x, num;
-	stack_t *temp;
+	int temp;
 
-	temp = *stack;
-	x = 0;
-	while (temp != NULL)
+	if (*stack == NULL || ((*stack)->next == NULL))
 	{
-		temp = temp->next;
-		++x;
-	}
-	if (x < 2)
-	{
-		printf("L%u: can't swap, stack too short\n", line_number);
+		printf("L%d: can't swap, stack too short", line_number);
 		exit(EXIT_FAILURE);
 	}
-	num = (*stack)->n;
+
+	temp = (*stack)->n;
 	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = num;
+	(*stack)->next->n = temp;
 }
