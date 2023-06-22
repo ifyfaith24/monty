c1546924be6de60f9d6f5e36664f29e1720ca7dc
ifyfaith24 <ifyfaith24@gmail.com>
2023-06-22 13:36:25 -0700

    monty project

--- nop.c
+++ nop.c
@@ -1,12 +1,25 @@
 #include "monty.h"
 
 /**
- * nop - a function that does nothing
- * @stack: double pointer to stack
- * @line_number: current error line
+ * nop_func - function to catch newline, empty line, or nop
+ * @stack: input pointer to head of stack
+ * @line_number: line count currently reading instructions from
  */
-void nop(stack_t **stack, unsigned int line_number)
+
+void nop_func(stack_t **stack, unsigned int line_number)
 {
-	stack--;
-	line_number--;
+	(void)stack;
+	(void)line_number;
+
+	free(global.opcode);
+	global.opcode = malloc(sizeof(char) * 2);
+	if (global.opcode == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		fclose(global.fd);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	global.opcode[0] = '\n';
+	global.opcode[1] = '\0';
 }
